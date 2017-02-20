8e28a63050dc6e98cbe7ca7779993f396a3d96c9
bstem <CEntzminger@bstem.biz>
2017-02-20 16:59:31 -0600

    Django Tutorial 1-5
    Django Polls Tutorial 1-5 completed

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,50 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.utils import timezone
+from django.views import generic
 
-def index(request):
-    return HttpResponse("Hello, user. You're at the polls index.")
+from .models import Question, Choice
+
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions not published yet
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # if error, redisplay question voting form
+        context = {
+            'question': question,
+            'error_message': "You didn't select a choice",
+        }
+        return render(request, 'polls/detail.html', context)
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # use Redirect when successfully dealing with POST data
+        #   - this prevents data from being posted twice if a user hits Back Button
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
